497855c1dee29852508de70e886a3970044e5ceb
jayden77lee <jayden77.lee@gmail.com>
2020-06-12 14:07:47 +1000

    branch demo.php updated

--- demo.php
+++ demo.php
@@ -1,7 +1,8 @@
 <?php include ('../demo_common.php'); ?>
 
 <?php
-	
+        echo 'Jayden Test';
+
 	echo '<iframe class="full-screen-preview__frame" src="https://jaydenweb.com.au/demo/arch/" name="preview-frame" frameborder="0" noresize="noresize" data-view="fullScreenPreview" style="height: 908px;">
 		</iframe>
 </body>
