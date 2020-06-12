94ffdf998a890a5c9675b705c751d2fb5adcaa3b
jayden77lee <jayden77.lee@gmail.com>
2020-06-12 11:28:06 +1000

    1st commit

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php include ('../demo_common.php'); ?>
+
+<?php
+	
+	echo '<iframe class="full-screen-preview__frame" src="https://jaydenweb.com.au/demo/arch/" name="preview-frame" frameborder="0" noresize="noresize" data-view="fullScreenPreview" style="height: 908px;">
+		</iframe>
+
+
+</body>
+
+</html>'
+?>
---
+++ demo_common.php
@@ -0,0 +1,90 @@
+<?php
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" id="architect_pro-gfonts-robotocondensed-css" href="//fonts.googleapis.com/css?family=Roboto+Condensed%3A400%2C300%2C300italic%2C400italic%2C700%2C700italic&amp;ver=4.7.2" type="text/css" media="all">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">	
+	<style>
+
+		.full-screen-preview {
+		   	height: 100%;
+		   	padding: 0px;
+		   	margin: 0px;
+		   	overflow: hidden;
+		   	font-family: Roboto Condensed;
+		}
+		.full-screen-preview__frame {
+ 		   	width: 100%;
+    		background-color: white;
+		}
+		iframe {
+    		border: 0;
+		}
+		.preview__header{
+			height: 100px;
+    		border-bottom: 3px solid darkorange;
+		}
+		.preview__header2 {
+			max-width: 1200px;
+    	    padding-left: 10px;
+    		padding-right: 10px;
+    		margin-left: auto;
+    		margin-right: auto
+		}
+		.preview__vteq-logo {
+ 			float: left;
+    		padding: 0 20px;
+		}
+		.preview__center_word {
+ 			float: left;
+    		padding: 0 20px;
+		}
+		.center_word {
+			font: 500 36px/36px "Roboto Condensed";
+			color: darkcyan;
+		}
+		.preview__actions {
+	    	float: left;
+	    	padding-top: 15px;
+		}
+		.jaydenweb_logo {
+			width: 300px;
+    		padding: 10px;
+		}
+		.headerinfo {
+		    float: right;
+		    font: 500 18px/44px "Roboto Condensed";
+		    color: #000;
+		    padding: 10px;
+		}
+		.headerinfo, .headerinfo a {
+			color: #000;
+		}
+
+	</style>	
+	
+
+<title>JaydenWEB Demo Websites</title>
+</head>
+
+<body class="full-screen-preview">
+
+		<div class="preview__header" data-view="ctaHeader">
+			<div class="preview__header2">
+
+
+			  	<div class="preview__vteq-logo">
+			    	<a href="https://jaydenweb.com.au"><img class="jaydenweb_logo" src="https://jaydenweb.com.au/wp-content/uploads/2019/03/logo_h.png"></a>
+			  	</div>
+
+			  	<div class="preview__center_word">
+			   		 <h1 class="center_word"> Business Website from $1,100 <span style="font-size:18px;">(valued at $2,200) </span></h1>
+			   		 <div style="font-size:14px;margin-top: -20px;color:darkcyan;">*Conditions applied.  <i class="fa fa-phone"></i> 0422 118 165 <span style="padding-left: 10px;"><i class="fa fa-envelope"></i> <a href="mailto:info@jaydenweb.com.au">info@jaydenweb.com.au</a></span> </div>
+			  	</div>
+			</div>
+		</div>'
+
+?>
