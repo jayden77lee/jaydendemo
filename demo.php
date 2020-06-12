07feb807c29a15ba0750c52c35f764ca76caf715
jayden77lee <jayden77.lee@gmail.com>
2020-06-12 11:54:38 +1000

    demo.php line removal

--- demo.php
+++ demo.php
@@ -5,8 +5,6 @@
 	echo '<iframe class="full-screen-preview__frame" src="https://jaydenweb.com.au/demo/arch/" name="preview-frame" frameborder="0" noresize="noresize" data-view="fullScreenPreview" style="height: 908px;">
 		</iframe>
 
-
 </body>
-
 </html>'
 ?>
