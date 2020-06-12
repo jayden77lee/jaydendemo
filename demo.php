fe26c7cde23dfb07059657f1c15ec159292702f4
jayden77lee <jayden77.lee@gmail.com>
2020-06-12 14:25:51 +1000

    branch new1

--- demo.php
+++ demo.php
@@ -1,8 +1,9 @@
 <?php include ('../demo_common.php'); ?>
 
 <?php
-        echo 'Jayden Test';
-        echo 'branch v2';
+echo 'Jayden Test';
+echo 'branch v2';
+	echo 'new1';
 	echo '<iframe class="full-screen-preview__frame" src="https://jaydenweb.com.au/demo/arch/" name="preview-frame" frameborder="0" noresize="noresize" data-view="fullScreenPreview" style="height: 908px;">
 		</iframe>
 </body>
